fbced3e9e5cb9b1284372ff8eab7467152a8238d
LAB-USR-CHICLAYO <LAB-USR-CHICLAYO@grupoutp.net>
2025-11-13 13:12:38 -0500

    a

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,19 +3,6 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
-    rules: {
-      semi: ["error", "always"],       // obliga punto y coma
-      quotes: ["error", "double"],     // obliga comillas dobles
-      "no-var": "error"                // prohíbe 'var'
-    }
-  },
-  {
-    files: ["**/*.js"],
-    languageOptions: { sourceType: "script" }
-  }
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
 ]);
